f167586f72a5e77ef4a99b1177b4cbc536e00b75
Gustavo J Gallardo <ggallard@identicum.com>
2026-04-14 21:21:02 -0300

    new version scheme for releases

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='sherpa-py-ldap',
-    version='1.0.20260228',
+    version='1.1.0',
     description='Python LDAP utilities',
     url='git@github.com:Identicum/sherpa-py-ldap.git',
     author='Identicum',
