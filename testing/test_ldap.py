c86793d24cc9a393aff380a916e9fd54522eab4e
Gustavo J Gallardo <ggallard@identicum.com>
2026-09-19 15:11:51 -0300

    Update test_ldap.py

--- testing/test_ldap.py
+++ testing/test_ldap.py
@@ -15,17 +15,15 @@ def main():
 	logger.info("{} finished.".format(os.path.basename(__file__)))
 
 
-def create_ldap_objects(ldap, base_dn, users_base_dn, groups_base_dn):
-	ldap.create_ad_ou(base_dn, "sherpa_users", ignore_if_exists=True)
-	ldap.create_ad_ou(base_dn, "sherpa_groups", ignore_if_exists=True)
+def create_ldap_objects(ldap: LDAP, base_dn: str, users_base_dn: str, groups_base_dn: str):
+	ldap.create_ad_ou(base_dn=base_dn, name="sherpa_users", ignore_if_exists=True)
+	ldap.create_ad_ou(base_dn=base_dn, name="sherpa_groups", ignore_if_exists=True)
 	group_members = []
-	ldap.create_ad_user(users_base_dn, f"testuser000", "testPassword.2025", f"testuser000@idsherpa.com", "Test000", "User000", employee_id="000", title="title000", employee_type="contractor", ignore_if_exists=True)
-	ldap.create_ad_user(users_base_dn, f"testuser001", "testPassword.2025", f"testuser001@idsherpa.com", "Test001", "User001", employee_id="001", department="department001", ignore_if_exists=True)
 	for i in range(2):
-		ldap.create_ad_user(users_base_dn, f"testuser{i}", "testPassword.2025", f"testuser{i}@idsherpa.com", "Test", "User1", ignore_if_exists=True)
+		ldap.create_ad_user(base_dn=users_base_dn, username=f"testuser{i}", password="testPassword.2025", upn=f"testuser{i}@idsherpa.com", given_name="Test", last_name="User1", ignore_if_exists=True)
 		group_members.append((f"cn=testuser{i},{users_base_dn}").encode())
-	ldap.create_ad_group(groups_base_dn, "testgroup1", group_members, ignore_if_exists=True)
-	ldap.create_ad_group(groups_base_dn, "testgroup2", group_members, ignore_if_exists=True)
+	for i in range(2):
+		ldap.create_ad_group(base_dn=groups_base_dn, name=f"testgroup{i}", description=f"Test group {i}", members=group_members, ignore_if_exists=True)
 
 
 def run(logger):
@@ -39,12 +37,15 @@ def run(logger):
 	users_base_dn = "ou=sherpa_users,{}".format(base_dn)
 	groups_base_dn = "ou=sherpa_groups,{}".format(base_dn)
 
-	create_ldap_objects(ldap, base_dn, users_base_dn, groups_base_dn)
+	create_ldap_objects(ldap=ldap, base_dn=base_dn, users_base_dn=users_base_dn, groups_base_dn=groups_base_dn)
 
-	for object in ldap.get_objects(users_base_dn, filter="(objectclass=user)", page_size=20):
+	for object in ldap.get_objects(base_dn=users_base_dn, filter="(objectclass=user)", page_size=20):
 		cvs_row = ldap.get_attributes_csv(object, attr_list=["cn","sn","givenName","employeeID","title", "employeeType", "memberOf", "department"], multivalue_separator="##")
 		logger.info("CSV row: {}", cvs_row)
-	ldap.get_object("ou=sherpa_groups,dc=idsherpa,dc=com")
+
+	ldap.get_object(object_dn="ou=sherpa_groups,dc=idsherpa,dc=com")
+
+	ldap.get_object(object_dn="cn=testgroup1,ou=sherpa_groups,dc=idsherpa,dc=com")
 
 
 if __name__ == "__main__":
